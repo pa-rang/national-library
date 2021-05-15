8997bbc0d70f4de67f58c241ea9f1665ab574fd6
pa-rang <appdler96@kaist.ac.kr>
2021-05-15 17:20:30 +0900

    emotion 설치 및 styled 적용

---
+++ pages/user/login.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+const LoginTitle = styled.div`
+  color: red;
+`;
+
+const mockData = {
+  title: "디지털 열람석",
+};
+
+function Login() {
+  return (
+    <div>
+      <LoginTitle>{mockData.title}</LoginTitle>
+    </div>
+  );
+}
+
+export default Login;
