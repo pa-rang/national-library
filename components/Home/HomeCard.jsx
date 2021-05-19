5444a87dae2a82ee95acf1aff6421c3e214520be
shji023 <shyunji023@gmail.com>
2021-05-20 02:26:50 +0900

    feat: header->navbar, Homepage->index, HomeCard 컴포넌트화 style: 전반적인 반응형 작업 추가

--- components/Home/HomeCard.jsx
+++ components/Home/HomeCard.jsx
@@ -1,6 +1,10 @@
+import React from 'react';
 import styled from "@emotion/styled";
+import StyledButton from '../common/StyledButton';
+import { useRouter } from "next/router";
+import Image from "next/image";
 
-const HomeCard = styled.div`
+const HomeCardWrapper = styled.div`
   width: 154px;
   height: 154px;
   margin: 0 auto;
@@ -15,6 +19,71 @@ const HomeCard = styled.div`
   border-radius: 12px;
   box-shadow: rgba(14, 30, 37, 0.12) 0px 2px 4px 0px, 
     rgba(14, 30, 37, 0.32) 0px 2px 16px 0px;
+  .card__image {
+    
+  }
+  .card__title {
+    font-size: 16px;
+    color: #011E57;
+    font-weight: bold;
+  }
+  .card__desc {
+    font-size: 12px;
+    color: #011E57;
+  }
+  .card__button {
+    display: none;
+  }
+  @media screen and (min-width: 1000px) {
+    width: 278px;
+    height: 278px;
+    .card__image img{
+      width: 130px;
+      height: 130px;
+    }
+    .card__title {
+      font-size: 24px;
+      color: #011E57;
+      font-weight: bold;
+      margin: 10px auto;
+    }
+    .card__desc {
+      font-size: 12px;
+      color: #011E57;
+      margin-bottom: 10px;
+    }
+    .card__button {
+      display: block;
+    }
+  }
 `;
 
+function HomeCard({card}) {
+  const router = useRouter();
+  const {image, title, desc, button, clickable} = card;
+  let btnColor = "navy";
+  if (clickable===false){
+    btnColor = "gray";
+  }
+  return(
+    <>
+    <HomeCardWrapper>
+      <div className="card__image">
+      <img
+        width='68px' 
+        height='68px' 
+        src={image} 
+        onClick={() => router.push("/reservation")}
+      />
+      </div>
+      <div className="card__title">{title}</div>
+      <div className="card__desc">{desc}</div>
+      <div className="card__button">
+        <StyledButton appearance={btnColor}>{button}</StyledButton>
+      </div>
+    </HomeCardWrapper>
+    </>
+  );
+
+}
 export default HomeCard;
