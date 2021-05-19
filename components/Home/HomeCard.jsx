a3fefb46f49bcb0a1170ca65b483d0eb4c04efee
shji023 <shji0@naver.com>
2021-05-19 10:17:17 +0900

    feat: HomePage에 HomeCard추가 style: HomeCard 반응형작업

---
+++ components/Home/HomeCard.jsx
@@ -0,0 +1,20 @@
+import styled from "@emotion/styled";
+
+const HomeCard = styled.div`
+  width: 154px;
+  height: 154px;
+  margin: 0 auto;
+  padding: 10px 5px;
+  display: flex;
+  flex-direction: column; 
+  align-items: center;
+  justify-content: flex-start;
+  box-sizing: border-box;
+  color: black;
+  background-color: white;
+  border-radius: 12px;
+  box-shadow: rgba(14, 30, 37, 0.12) 0px 2px 4px 0px, 
+    rgba(14, 30, 37, 0.32) 0px 2px 16px 0px;
+`;
+
+export default HomeCard;
--- pages/HomePage.jsx
+++ pages/HomePage.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useRouter } from "next/router";
 import Header from '../components/common/Header';
+import HomeCard from '../components/Home/HomeCard';
 import MainTop from '../components/Home/MainTop';
+import Image from "next/image";
 import styled from "@emotion/styled";
 
 const HomeWrap = styled.div`
@@ -45,6 +48,32 @@ const HomeText = styled.p`
   }
 `;
 
+const CardWrap = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, auto);
+  row-gap: 12px;
+  @media (min-width: 375px) {
+    margin-top:40px;
+    grid-template-columns: repeat(3, auto);
+  }
+  @media (min-width: 1000px) {
+    margin-top:40px;
+    grid-template-columns: repeat(4, auto);
+  }
+`;
+
+const ImageStyle = styled(Image)`
+  width: 68px;
+  height: 68px;
+  cursor: pointer;
+`;
+
+const CardText = styled.p`
+  color: #011E57;
+  margin: 0;
+  ${({ size = 'LARGE' }) => sizeSet[size]}
+`;
+
 const userData = {
     id:'1',
     nickname:'안소현',
@@ -53,7 +82,9 @@ const userData = {
 };
 
 function HomePage() {
+  const router = useRouter();
   const {nickname, photoURl, reservation} = userData;
+
     return (
         <>
         <Header nickname={nickname} photoURl={photoURl} ></Header>
@@ -68,6 +99,37 @@ function HomePage() {
             코로나 19 감염예방을 위해 <span style={{color:'#FEC108'}}>제한적 운영</span> 중입니다.
           </HomeText>
         </HomeWrap>
+
+        <CardWrap>
+          <HomeCard>
+            <ImageStyle width={68} height={68} src={"/assets/icons/Digital.svg"}/>
+            <CardText size="LARGE">디지털 열람석</CardText><CardText size="SMALL">디지털자료 열람 및 문서 편집 공간</CardText>
+          </HomeCard>
+          <HomeCard>
+            <ImageStyle width={68} height={68} src={"/assets/icons/DB.svg"}/>
+            <CardText size="LARGE">원문 DB석</CardText><CardText size="SMALL">원문 DB 열람 전용 공간</CardText>
+          </HomeCard>
+          <HomeCard>
+            <ImageStyle width={68} height={68} src={"/assets/icons/Editor.svg"}/>
+            <CardText size="LARGE">미디어 편집석</CardText><CardText size="SMALL">영상, 음향, 이미지 콘텐츠 편집 공간</CardText>
+          </HomeCard>
+          <HomeCard>
+            <ImageStyle width={68} height={68} src={"/assets/icons/Media.svg"}/>
+            <CardText size="LARGE">미디어자료이용석</CardText><CardText size="SMALL">멀티미디어콘텐츠열람공간 자료3점 이용가능</CardText>
+          </HomeCard>
+          <HomeCard>
+            <ImageStyle width={68} height={68} src={"/assets/icons/Studio.svg"} onClick={() => router.push("/reservation")}/>
+            <CardText size="LARGE">스튜디오</CardText><CardText size="SMALL">영상, 음향등 미디어 콘텐츠 창작, 공유 공간</CardText>
+          </HomeCard>
+          <HomeCard>
+            <ImageStyle width={68} height={68} src={"/assets/icons/Laptop.svg"}/>
+            <CardText size="LARGE">노트북이용석</CardText><CardText size="SMALL">1인 1석 본인노트북 사용 (노트북 대여 없음)</CardText>
+          </HomeCard>
+          <HomeCard>
+            <ImageStyle width={68} height={68} src={"/assets/icons/Seminar.svg"}/>
+            <CardText size="LARGE">세미나실</CardText><CardText size="SMALL">그룹별 세미나 공간</CardText>
+          </HomeCard>
+        </CardWrap>
         </>
     );
 };
