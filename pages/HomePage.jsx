56eb8c3a421725c2f6f1b396e428616f73fe4d21
shji023 <shji0@naver.com>
2021-05-19 10:21:12 +0900

    feat: HomeCard 렌더링 방식 변경

--- pages/HomePage.jsx
+++ pages/HomePage.jsx
@@ -81,10 +81,65 @@ const userData = {
     reservation:1,
 };
 
-function HomePage() {
-  const router = useRouter();
-  const {nickname, photoURl, reservation} = userData;
+const mockData = [
+  {
+      id:"1",
+      image:"/assets/icons/Digital.svg",
+      title: "디지털 열람석",
+      desc:"디지털자료 열람 및 문서 편집 공간",
+  },
+  {
+      id:"2",
+      image:"/assets/icons/DB.svg",
+      title: "원문 DB석",
+      desc:"원문 DB 열람 전용 공간",
+  },
+  {
+      id:"3",
+      image:"/assets/icons/Editor.svg",
+      title: "미디어 편집석",
+      desc:"영상, 음향, 이미지 콘텐츠 편집 공간",
+  },
+  {
+      id:"4",
+      image:"/assets/icons/Media.svg",
+      title: "미디어자료이용석",
+      desc:"멀티미디어 콘텐츠 열람 공간 자료3점 이용가능",
+  },
+  {
+      id:"5",
+      image:"/assets/icons/Studio.svg",
+      title: "스튜디오",
+      desc:"영상, 음향등 미디어 콘텐츠 창작, 공유 공간",
+  },
+  {
+      id:"6",
+      image:"/assets/icons/Laptop.svg",
+      title: "노트북이용석",
+      desc:"1인 1석 본인노트북 사용 (노트북 대여 없음)",
+  },
+  {
+      id:"7",
+      image:"/assets/icons/Seminar.svg",
+      title: "세미나실",
+      desc:"그룹별 세미나 공간",
+  },
+]
 
+function Temp ({card}){
+  const router = useRouter();
+  return(
+    <>
+    <HomeCard>
+      <ImageStyle width={68} height={68} src={card.image} onClick={() => router.push("/reservation")}/>
+      <CardText size="LARGE">{card.title}</CardText><CardText size="SMALL">{card.desc}</CardText>
+    </HomeCard>
+    </>
+  );
+}
+
+function HomePage() {
+  const {nickname, photoURl, reservation} = userData;
     return (
         <>
         <Header nickname={nickname} photoURl={photoURl} ></Header>
@@ -101,34 +156,11 @@ function HomePage() {
         </HomeWrap>
 
         <CardWrap>
-          <HomeCard>
-            <ImageStyle width={68} height={68} src={"/assets/icons/Digital.svg"}/>
-            <CardText size="LARGE">디지털 열람석</CardText><CardText size="SMALL">디지털자료 열람 및 문서 편집 공간</CardText>
-          </HomeCard>
-          <HomeCard>
-            <ImageStyle width={68} height={68} src={"/assets/icons/DB.svg"}/>
-            <CardText size="LARGE">원문 DB석</CardText><CardText size="SMALL">원문 DB 열람 전용 공간</CardText>
-          </HomeCard>
-          <HomeCard>
-            <ImageStyle width={68} height={68} src={"/assets/icons/Editor.svg"}/>
-            <CardText size="LARGE">미디어 편집석</CardText><CardText size="SMALL">영상, 음향, 이미지 콘텐츠 편집 공간</CardText>
-          </HomeCard>
-          <HomeCard>
-            <ImageStyle width={68} height={68} src={"/assets/icons/Media.svg"}/>
-            <CardText size="LARGE">미디어자료이용석</CardText><CardText size="SMALL">멀티미디어콘텐츠열람공간 자료3점 이용가능</CardText>
-          </HomeCard>
-          <HomeCard>
-            <ImageStyle width={68} height={68} src={"/assets/icons/Studio.svg"} onClick={() => router.push("/reservation")}/>
-            <CardText size="LARGE">스튜디오</CardText><CardText size="SMALL">영상, 음향등 미디어 콘텐츠 창작, 공유 공간</CardText>
-          </HomeCard>
-          <HomeCard>
-            <ImageStyle width={68} height={68} src={"/assets/icons/Laptop.svg"}/>
-            <CardText size="LARGE">노트북이용석</CardText><CardText size="SMALL">1인 1석 본인노트북 사용 (노트북 대여 없음)</CardText>
-          </HomeCard>
-          <HomeCard>
-            <ImageStyle width={68} height={68} src={"/assets/icons/Seminar.svg"}/>
-            <CardText size="LARGE">세미나실</CardText><CardText size="SMALL">그룹별 세미나 공간</CardText>
-          </HomeCard>
+          {
+            mockData.map(
+              card => (<Temp card={card} key={card.id}/>)
+            )
+          }
         </CardWrap>
         </>
     );
