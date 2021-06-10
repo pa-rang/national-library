a9465ff41bcd97bbf88cc85fec4250f58734c5bc
healing99 <nicealice99@gmail.com>
2021-06-10 23:17:08 +0900

    feat: 스튜디오 예약 페이지 button click 이벤트 설정

--- components/reservation/DateButton.jsx
+++ components/reservation/DateButton.jsx
@@ -1,34 +1,42 @@
-import styled from "@emotion/styled";
+import styled from '@emotion/styled';
+import { useState } from 'react';
 
 const DateButtonWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 33px;
-  margin-bottom: 2px;
-  margin-right: 19px;
-  min-width: 67px;
-  border-radius: 10px;
-  font-weight: 500;
-  font-size: 12px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	height: 33px;
+	margin-bottom: 2px;
+	margin-right: 19px;
+	min-width: 67px;
+	border-radius: 10px;
+	font-weight: 500;
+	font-size: 12px;
 
-  border: ${(props) => (props.isSelected ? "none" : "1px solid #011e57")};
-  background-color: ${(props) => (props.isSelected ? "#25C1F3" : "#fff")};
-  color: ${(props) => (props.isSelected ? "#fff" : "#011e57")};
-  cursor: pointer;
+	border: ${props => (props.isClicked ? 'none' : '1px solid #011e57')};
+	background-color: ${props => (props.isClicked ? '#25C1F3' : '#fff')};
+	color: ${props => (props.isClicked ? '#fff' : '#011e57')};
+	cursor: pointer;
 
-  @media screen and (min-width: 1000px) {
-    width: 133px;
-    height: 57px;
-    font-size: 21px;
-    margin-right: 24px;
-  }
+	@media screen and (min-width: 1000px) {
+		width: 133px;
+		height: 57px;
+		font-size: 21px;
+		margin-right: 24px;
+	}
 `;
 
-const DateButton = ({ isSelected }) => {
-  return (
-    <DateButtonWrapper isSelected={isSelected}>05-17(월)</DateButtonWrapper>
-  );
+const DateButton = ({ date }) => {
+	const [isClicked, setIsClicked] = useState(false);
+	const handleDateClick = () => {
+		setIsClicked(val => !val);
+	};
+
+	return (
+		<DateButtonWrapper isClicked={isClicked}>
+			<div onClick={handleDateClick}> {date}</div>
+		</DateButtonWrapper>
+	);
 };
 
 export default DateButton;
